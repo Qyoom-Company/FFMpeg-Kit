b7037cb126978c0b0cd64306cb373edc70f5c0f6
Taha Hussein <taha.hussein.two@gmail.com>
2024-05-01 11:42:36 +0300

    Initial Commit

---
+++ Tests/ffmpeg-kit-spm-mainTests/ffmpeg_kit_spm_mainTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ffmpeg_kit_spm_main
+
+final class ffmpeg_kit_spm_mainTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
